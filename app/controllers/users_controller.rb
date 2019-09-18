46049812d24a536f0c797949a0df7d870ae4e2de
Stuart Hahn <github email address>
2019-09-18 13:04:23 -0400

    add post login route to take login params and authenticate user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,15 @@ class UsersController < ApplicationController
     get '/login' do
         erb :login
     end
+
+    #take the login params and validate them
+    post '/login' do
+        @user = User.find_by(email: params[:email])
+        if @user.authenticate(params[:password])
+            redirect "/users/#{@user.id}"
+        else
+            redirect "/login"
+        end
+    end
+
 end
